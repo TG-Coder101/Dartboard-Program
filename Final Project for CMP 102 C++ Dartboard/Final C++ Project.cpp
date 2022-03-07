60cda5576204fceef266dee4f862d8264ada9cef
Tom G <58516757+TG-Coder101@users.noreply.github.com>
2022-03-07 22:08:55 +0000

    Update Final C++ Project.cpp

--- Final Project for CMP 102 C++ Dartboard/Final C++ Project.cpp
+++ Final Project for CMP 102 C++ Dartboard/Final C++ Project.cpp
@@ -8,13 +8,11 @@
 
 using namespace std;
 
-
 void games()  //10000, 13sets, 5 games
 {
 
 	string name0 = "Joe";
 	string name1 = "Sid";
-
 
 	Player Players[2]; //creates an array of 
 
@@ -54,47 +52,20 @@ void games()  //10000, 13sets, 5 games
 					for (int p = 0; p < 2 && !done; p++) // if done isnt true it runs again
 					{
 						//cout << Players[p].name << endl;
-
 						for (int q = 0; q < 3 && !done; q++)
 						{
 							Players[p].playerthrow(dartboard);
-
 							//cout << Players[p].score << endl;
-
-
 							if (Players[p].score == 0)
 							{
 								done = true; //if score = 0 after throw then it is done
 								Players[p].Scores_games++;
-
-								/*if (Players[p].Scores_games == 3)
-								{
-								Players[p].Scores_games = 0;
-								Players[p].Scores_sets++;
-
-								if (Players[p].Scores_sets == 7)
-								{
-								Players[p].Scores_sets = 0;
-								Players[p].Scores_matches++;
-
-
-
-								}
-
-								}*/
-
-
 							}
-
 						}
-
 					}
-
 				} while (!done); //equal to done
-
 			}
 		}
-
 	}
 
 	cout << n_games << endl;
@@ -109,17 +80,13 @@ int main() //main
 
 	games();//function prototype 
 
-
 	Player Players[2]; //creates an array of player objects
 
 	srand(time(0)); //random number generator
 
-
 	Players[0].name = "Joe";
 	Players[0].chance = 71;
 	Players[0].score = 501;
-
-
 
 	Players[1].name = "Sid";
 	Players[1].chance = 73;
@@ -127,9 +94,7 @@ int main() //main
 
 	Dartboard dartboard;
 
-
 	system("pause");
 
 	return 0;
-
 }
